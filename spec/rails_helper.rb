8474b56748d789de838ba4ab76edefa0f5ab8baa
vanessasouza142 <vanessa.kbs16@gmail.com>
2023-10-17 19:19:23 -0300

    Visitante vê a tela inicial

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,7 +31,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   config.before(type: :system) do
-    driven_by(:rack_text)
+    driven_by(:rack_test)
   end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
---
+++ spec/system/user_view_homepage_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'Usuário visita tela inicial' do
+    it 'e vê o nome da app' do
+        #Arrange
+
+        #Act
+        visit('/')
+        #Assert
+        expect(page).to have_content('Galpões & Estoque')
+    end
+end
